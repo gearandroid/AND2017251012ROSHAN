754ddd1231980c31a8a36d758f92d38ec7fa269a
Roshan B <rbtusk@gmail.com>
2017-05-30 15:58:56 +0530

    Activity Lifecycle: Basic methods

---
+++ activitylifecycle/src/main/java/com/codekul/roshan/activitylifecycle/MainActivity.java
@@ -0,0 +1,55 @@
+package com.codekul.roshan.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mt("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mt("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mt("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mt("onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        mt("onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mt("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mt("onDestroy");
+    }
+
+    public  void mt(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show();
+    }
+}
--- listviewexample3/src/main/java/com/codekul/roshan/listviewexample3/MyItem.java
+++ listviewexample3/src/main/java/com/codekul/roshan/listviewexample3/MyItem.java
@@ -5,4 +5,11 @@ package com.codekul.roshan.listviewexample3;
  */
 
 public class MyItem {
+    public String text;
+    public int imgId;
+
+    public MyItem(String text,int imgId){
+        this.text=text;
+        this.imgId =imgId;
+    }
 }
