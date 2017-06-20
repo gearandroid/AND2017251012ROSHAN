4d15d573a7d456244fe7d448268e16cacc5f75bb
Roshan B <rbtusk@gmail.com>
2017-06-20 11:26:20 +0530

    Service: Playing media

---
+++ services/src/main/java/gonotepad/services/MyIntentService.java
@@ -0,0 +1,19 @@
+package gonotepad.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+
+public class MyIntentService extends IntentService {
+
+
+   public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
---
+++ services/src/main/java/gonotepad/services/MyService.java
@@ -0,0 +1,43 @@
+package gonotepad.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+
+public class MyService extends Service {
+
+    private MediaPlayer player;
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                player = MediaPlayer.create(MyService.this,R.raw.pal);
+                player.start();
+            }
+        }).start();
+
+        return START_NOT_STICKY;
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+        player.stop();
+        player.release();
+        super.onDestroy();
+    }
+}
